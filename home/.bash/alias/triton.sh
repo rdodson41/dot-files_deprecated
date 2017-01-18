88f07f809ff19f61f7bc8dfd0ccbf24352197044
Richard E. Dodson <richard.elias.dodson@gmail.com>
2017-01-17 21:54:18 -0500

    Escape Triton CLI profile exec

--- home/.bash/alias/triton.sh
+++ home/.bash/alias/triton.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for profile in $(triton profiles -H -o name 2> /dev/null | grep --invert-match env); do
-  alias "${profile}-exec"="TRITON_PROFILE=${profile} triton-profile-exec"
+  alias "${profile}-exec"="TRITON_PROFILE=\"${profile}\" triton-profile-exec"
   complete -c "${profile}-exec"
 done
 
