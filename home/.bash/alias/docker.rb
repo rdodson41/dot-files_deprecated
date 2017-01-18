fdbb63112528a36572b758c88ce4f98132d9989c
Richard E. Dodson <richard.elias.dodson@gmail.com>
2017-01-17 22:22:01 -0500

    Update Docker aliases

--- home/.bash/alias/docker.rb
+++ home/.bash/alias/docker.rb
@@ -2,23 +2,15 @@
 
 require 'yaml'
 
-FILE = File.join(ENV['HOME'], '.docker', 'services.yml')
-
-YAML.load_file(FILE).each do |application, services|
+YAML.load(ARGF.read).each do |application, services|
   name = application.delete(?-)
 
-  puts <<~EOF
-    alias #{application}-ps='docker ps --filter name=#{name}'
-  EOF
+  puts "alias #{application}-ps='docker ps --filter name=#{name}'"
 
   services.each do |service|
-    puts <<~EOF
-      alias #{application}-#{service}-ps='docker ps --filter name=#{name}-#{service}'
-      alias #{application}-#{service}='#{application}-#{service}-ps --quiet | xargs docker'
-      alias #{application}-#{service}-inspect-ip-address='#{application}-#{service} inspect --format {{.NetworkSettings.IPAddress}}'
-      alias #{application}-#{service}-inspect-ports='#{application}-#{service} inspect --format {{.NetworkSettings.Ports}}'
-      alias #{application}-#{service}-first='#{application}-#{service}-ps --quiet | first | xargs docker'
-      alias #{application}-#{service}-first-exec='docker exec --interactive --tty "$(#{application}-#{service}-ps --quiet | first)"'
-    EOF
+    puts "alias #{application}-#{service}-ps='docker ps --filter name=#{name}-#{service}'"
+    puts "alias #{application}-#{service}='#{application}-#{service}-ps --quiet | xargs docker'"
+    puts "alias #{application}-#{service}-first='#{application}-#{service}-ps --quiet | first | xargs docker'"
+    puts "alias #{application}-#{service}-first-exec='docker exec --interactive --tty \"$(#{application}-#{service}-ps --quiet | first)\"'"
   end
-end if File.exists?(FILE)
+end
