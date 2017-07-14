3bee1e7a7529dddeb576fbfaf7f97b1b94113472
Richard E. Dodson <richard.elias.dodson@gmail.com>
2017-07-14 19:42:30 -0400

    Remove docker aliases

--- home/.bash/alias/docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -f "${HOME}/.bash/alias/docker.jq" && -f "${HOME}/.docker/services.json" ]]; then
-  eval "$(jq --from-file "${HOME}/.bash/alias/docker.jq" --raw-output < "${HOME}/.docker/services.json" 2> /dev/null)"
-fi
