5ae16e0e0267b114a65109595d5f6973223ad023
Richard E. Dodson <richard.elias.dodson@gmail.com>
2017-07-14 20:31:26 -0400

    Remove Triton aliases and completion

--- home/.bash/alias/triton.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -f "${HOME}/.bash/alias/triton.jq" ]]; then
-  eval "$(triton profiles --json 2> /dev/null | jq --from-file "${HOME}/.bash/alias/triton.jq" --raw-output 2> /dev/null)"
-fi
